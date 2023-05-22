0f26c75d2987be797c4a0b93324511e40d4a0eb9
alexa <alex@trirecords.eu>
2023-05-22 18:43:12 +0200

    add support for discord webhooks

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { document } = window;
 
 require('dotenv').config();
 window.navigator.language = process.env.LANG || "en-US";
+
 const callbackUrl = process.env.CALLBACK_URL || undefined;
 if (!callbackUrl) {
     console.warn("CALLBACK_URL is not defined! Please define it in .env file.");
@@ -24,16 +25,40 @@ fetch(url).then(async res => {
     console.log("Setting up virtual site...");
     const $ = require("jquery")(window);
     $(document).ready(() => {
+        const oldConsoleLogs = disableLogs();
         $("body").html(text);
+        enableLogs(oldConsoleLogs);
         console.log("Site created!");
         setTimeout(() => {
-            getAvailableVersions($);
-            process.exit(0);
+            getAvailableVersions($).then(() => {
+                process.exit(0);
+            });
         }, 10000);
     });
 });
 
-function getAvailableVersions($) {
+function enableLogs(oldConsoleLogs) {
+    console.log = oldConsoleLogs.log;
+    console.warn = oldConsoleLogs.warn;
+    console.error = oldConsoleLogs.error;
+    console.debug = oldConsoleLogs.debug;
+}
+
+function disableLogs() {
+    const oldConsoleLogs = {
+        log: console.log,
+        warn: console.warn,
+        error: console.error,
+        debug: console.debug
+    };
+    console.log = function() {};
+    console.warn = function() {};
+    console.error = function() {};
+    console.debug = function() {};
+    return oldConsoleLogs;
+}
+
+async function getAvailableVersions($) {
     const reservationCtn = $(".reservation_ctn");
     console.log("Found " + reservationCtn.length + " versions.");
     const versions = [];
@@ -47,22 +72,44 @@ function getAvailableVersions($) {
     }
     console.log("Versions: ", versions);
     if (versions.find(v => v.available)) {
-        if (!callbackUrl) {
+        if (!callbackUrl || callbackUrl.length === 0) {
             console.warn("CALLBACK_URL is not defined! Skipping callback.");
             return;
         }
-        fetch(callbackUrl, {
+        const res = await fetch(callbackUrl, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(versions)
-        }).then(r => {
-            if (r.ok) {
-                console.log("Callback succeeded!");
-            } else {
-                console.error("Callback failed! status: " + r.status);
+            body: JSON.stringify(getCustomizedRequestBody(callbackUrl, versions))
+        })
+        if (res.ok) {
+            console.log("Callback succeeded!");
+        } else {
+            console.error("Callback failed! status: " + res.status);
+            console.log("Response: ", await res.text());
+        }
+    }
+}
+
+function getCustomizedRequestBody(callbackUrl, data) {
+    const baseDomain = callbackUrl.split("/").slice(0, 3).join("/");
+    switch (baseDomain) {
+        case "https://discord.com":
+            return {
+                content: "Steam Deck is available!",
+                embeds: [{
+                    title: "Steam Deck is available!",
+                    description: "Steam Deck is available! Go to [Steam Deck page](" + url + ") to buy it!",
+                    fields: [{
+                        name: "Versions",
+                        value: data.map(v => v.price).join("\n")
+                    }],
+                    color: 0x00ff00
+                }],
+                username: "Steam Deck Notifier"
             }
-        });
+        default:
+            return data;
     }
 }
