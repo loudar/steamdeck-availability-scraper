cf088602926aa544fd8a6c2b4d9b64a910cbdb5a
alexa <alex@trirecords.eu>
2023-05-22 18:21:30 +0200

    add .env for locale

--- server.js
+++ server.js
@@ -1,36 +1,71 @@
 const url = "https://store.steampowered.com/steamdeck";
+
+console.log("Starting... (this can take a little while)");
+const { JSDOM } = require("jsdom");
+const jsdom = new JSDOM('', { runScripts: "dangerously", resources: "usable", url });
+const { window } = jsdom;
+const { document } = window;
+
+require('dotenv').config();
+window.navigator.language = process.env.LANG || "en-US";
+const callbackUrl = process.env.CALLBACK_URL || undefined;
+if (!callbackUrl) {
+    console.warn("CALLBACK_URL is not defined! Please define it in .env file.");
+}
+
 fetch(url).then(async res => {
     if (!res.ok) {
-        console.log("fetching site failed, code: " + res.status);
+        throw new Error(`fetching site failed! status: ${res.status}`);
     } else {
-        console.log("fetching site succeeded!");
-        console.log("Creating jsdom...");
-        const { JSDOM } = require("jsdom");
-        const jsdom = new JSDOM('', { runScripts: "dangerously", resources: "usable", url: "https://store.steampowered.com/steamdeck" });
-        const { window } = jsdom;
-        const { document } = window;
-        
+        console.log("Fetching site succeeded.");
         console.log("Parsing text...");
-        const text = await res.text();
+        return res.text();
+}}).then(async text => {
+    console.log("Setting up virtual site...");
+    const $ = require("jquery")(window);
+    $(document).ready(() => {
+        const oldLog = console.log;
+        console.log = () => {};
+        $("body").html(text);
+        console.log = oldLog;
+        console.log("Site created!");
+        setTimeout(() => {
+            getAvailableVersions($);
+            process.exit(0);
+        }, 10000);
+    });
+});
 
-        console.log("Creating jquery...");
-        const $ = require("jquery")(window);
-        console.log("Inserting text into window...");
-        $(document).ready(() => {
-            setTimeout(() => {
-                $("body").html(text);
-                console.log("body created!");
-                setTimeout(() => {
-                    const nodes = $(".ReservationAddToCart button");
-                    console.log("Found " + nodes.length + " nodes.");
-                    if (nodes.length > 0) {
-                        for (let i = 0; i < nodes.length; i++) {
-                            const node = nodes[i];
-                            console.log(`version:${i}/${nodes.length}:${node.innerHTML}`);
-                        }
-                    }
-                }, 10000);
-            }, 100);
+function getAvailableVersions($) {
+    const reservationCtn = $(".reservation_ctn");
+    console.log("Found " + reservationCtn.length + " versions.");
+    const versions = [];
+    for (let rsvCtn of reservationCtn) {
+        const price = $(rsvCtn).find(".bbcode_price").find("div").text();
+        const available = $(rsvCtn).find(".ReservationAddToCart").length > 0;
+        versions.push({
+            price: price,
+            available: available
         });
     }
-});
+    console.log("Versions: ", versions);
+    if (versions.find(v => v.available)) {
+        if (!callbackUrl) {
+            console.warn("CALLBACK_URL is not defined! Skipping callback.");
+            return;
+        }
+        fetch(callbackUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(versions)
+        }).then(r => {
+            if (r.ok) {
+                console.log("Callback succeeded!");
+            } else {
+                console.error("Callback failed! status: " + r.status);
+            }
+        });
+    }
+}
